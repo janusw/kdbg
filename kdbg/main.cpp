ac18e90e5dd831a19aa6d87b85464e5893715a74
Janus Weil <janus@gcc.gnu.org>
2016-11-24 17:50:03 +0100

    KF5 port: get rid of more deprecation warnings * replace KApplication by QApplication * replace K4AboutData by KAboutData * replace KCmdLineArgs by QCommandLineParser

--- kdbg/main.cpp
+++ kdbg/main.cpp
@@ -4,13 +4,13 @@
  * See the file COPYING in the toplevel directory of the source directory.
  */
 
-#include <kapplication.h>
 #include <klocalizedstring.h>			/* i18n */
 #include <kmessagebox.h>
 #include <kglobal.h>
 #include <kstandarddirs.h>
-#include <kcmdlineargs.h> 
-#include <k4aboutdata.h>
+#include <kaboutdata.h>
+#include <QApplication>
+#include <QCommandLineParser>
 #include "dbgmainwnd.h"
 #include "typetable.h"
 #include "version.h"
@@ -20,38 +20,43 @@
 
 int main(int argc, char** argv)
 {
-    K4AboutData aboutData("kdbg", "kdbg", ki18n("KDbg"),
+    QApplication app(argc, argv);
+
+    KAboutData aboutData("kdbg", i18n("KDbg"),
 			 KDBG_VERSION,
-			 ki18n("A Debugger"),
-			 K4AboutData::License_GPL, 
-			 ki18n("(c) 1998-2016 Johannes Sixt"),
-			 KLocalizedString(),	/* any text */
+			 i18n("A Debugger"),
+			 KAboutLicense::GPL,
+			 i18n("(c) 1998-2016 Johannes Sixt"),
+			 QString(),	/* any text */
 			 "http://www.kdbg.org/",
 			 "j6t@kdbg.org");
-    aboutData.addAuthor(ki18n("Johannes Sixt"), KLocalizedString(), "j6t@kdbg.org");
-    aboutData.addCredit(ki18n("Keith Isdale"),
-			ki18n("XSLT debugging"),
+    aboutData.addAuthor(i18n("Johannes Sixt"), QString(), "j6t@kdbg.org");
+    aboutData.addCredit(i18n("Keith Isdale"),
+			i18n("XSLT debugging"),
 			"k_isdale@tpg.com.au");
-    aboutData.addCredit(ki18n("Daniel Kristjansson"),
-			ki18n("Register groups and formatting"),
+    aboutData.addCredit(i18n("Daniel Kristjansson"),
+			i18n("Register groups and formatting"),
 			"danielk@cat.nyu.edu");
-    aboutData.addCredit(ki18n("David Edmundson"),
-			ki18n("KDE4 porting"),
+    aboutData.addCredit(i18n("David Edmundson"),
+			i18n("KDE4 porting"),
 			"david@davidedmundson.co.uk");
-    KCmdLineArgs::init( argc, argv, &aboutData );
+    KAboutData::setApplicationData(aboutData);
 
-    KCmdLineOptions opts;
-    opts.add("t <file>", ki18n("transcript of conversation with the debugger"));
-    opts.add("r <device>", ki18n("remote debugging via <device>"));
-    opts.add("l <language>", ki18n("specify language: C, XSLT"));
-    opts.add("x", ki18n("use language XSLT (deprecated)"));
-    opts.add("a <args>", ki18n("specify arguments of debugged executable"));
-    opts.add("p <pid>", ki18n("specify PID of process to debug"));
-    opts.add("+[program]", ki18n("path of executable to debug"));
-    opts.add("+[core]", ki18n("a core file to use"));
-    KCmdLineArgs::addCmdLineOptions(opts);
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addOption(QCommandLineOption("t", i18n("transcript of conversation with the debugger"), "file"));
+    parser.addOption(QCommandLineOption("r", i18n("remote debugging via <device>"), "device"));
+    parser.addOption(QCommandLineOption("l", i18n("specify language: C, XSLT"), "language"));
+    parser.addOption(QCommandLineOption("x", i18n("use language XSLT (deprecated)")));
+    parser.addOption(QCommandLineOption("a", i18n("specify arguments of debugged executable"), "args"));
+    parser.addOption(QCommandLineOption("p", i18n("specify PID of process to debug"), "pid"));
+    parser.addPositionalArgument("program", i18n("path of executable to debug"));
+    parser.addPositionalArgument("core", i18n("a core file to use"));
 
-    KApplication app;
+    aboutData.setupCommandLine(&parser);
+    parser.process(app);
+    aboutData.processCommandLine(&parser);
 
     KGlobal::dirs()->addResourceType("types", "data", "kdbg/types");
     KGlobal::dirs()->addResourceType("sessions", "data", "kdbg/sessions");
@@ -75,16 +80,15 @@ int main(int argc, char** argv)
 
     // handle options
 
-    KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
-    QString transcript = args->getOption("t");
-    QString remote = args->getOption("r");
+    QString transcript = parser.value("t");
+    QString remote = parser.value("r");
     if (!remote.isEmpty())
 	debugger->setRemoteDevice(remote);
 
-    QString lang = args->getOption("l");
+    QString lang = parser.value("l");
 
     // deprecated option; overrides -l
-    if (args->isSet("x")){
+    if (parser.isSet("x")){
          /* run in xsldbg mode  */
          lang = "xslt";
     }
@@ -95,20 +99,21 @@ int main(int argc, char** argv)
     }
     debugger->setTranscript(transcript);
 
-    QString pid = args->getOption("p");
-    QString programArgs = args->getOption("a");
+    QString pid = parser.value("p");
+    QString programArgs = parser.value("a");
 
-    if (!restored && args->count() > 0) {
+    const QStringList args = parser.positionalArguments();
+    if (!restored && args.size() > 0) {
 	// attach to process?
 	if (!pid.isEmpty()) {
 	    TRACE("pid: " + pid);
 	    debugger->setAttachPid(pid);
 	}
 	// check for core file; use it only if we're not attaching to a process
-	else if (args->count() > 1 && pid.isEmpty()) {
-	    debugger->setCoreFile(args->arg(1));
+	else if (args.size() > 1 && pid.isEmpty()) {
+	    debugger->setCoreFile(args.at(1));
 	}
-	if (!debugger->debugProgram(args->arg(0), lang)) {
+	if (!debugger->debugProgram(args.at(0), lang)) {
 	    // failed
 	    TRACE("cannot start debugger");
 	    KMessageBox::error(debugger, i18n("Cannot start debugger."));
